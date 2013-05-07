f1b478d126f7e3c631607a2e037ff488386ce6f8
tarao <tarao.gnn@gmail.com>
2013-05-08 08:14:47 +0900

    Add library headers.
    This closes issue #2.

--- multi-mode+evil.el
+++ multi-mode+evil.el
@@ -1,3 +1,29 @@
+;;; multi-mode+evil.el --- Evil patch for multi-mode
+
+;; Copyright (C) 2010-2013 Lintaro INA
+
+;; Author: INA Lintaro <tarao.gnn at gmail.com>
+;; Version: 0.0.2
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (require 'multi-mode-util)
 (require 'evil)
 
